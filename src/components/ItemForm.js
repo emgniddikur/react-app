2c9b0351288c5a9b71138ae9e906ea931429880e
katoyoshiharu@team-lab.com <katoyoshiharu@team-lab.com>
2019-05-14 22:49:31 +0900

    [Fix] 商品登録後ルートURLに遷移

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export const ItemForm = ({formItem, inputTitle, inputDescription, inputPrice, addItem}) => {
   return (
@@ -10,7 +11,9 @@ export const ItemForm = ({formItem, inputTitle, inputDescription, inputPrice, ad
              onChange={e => inputDescription(e.target.value)}/><br/>
       <label htmlFor="price">価格</label>
       <input id="price" type="text" value={formItem.price} onChange={e => inputPrice(e.target.value)}/><br/>
-      <input type="button" value="新規作成" onClick={() => addItem(formItem)}/>
+      <Link to="/">
+        <input type="button" value="新規作成" onClick={() => addItem(formItem)}/>
+      </Link>
     </form>
   );
 };
