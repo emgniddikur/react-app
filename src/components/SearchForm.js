70cc505faee26df993d12819b39223885f5a07e3
katoyoshiharu@team-lab.com <katoyoshiharu@team-lab.com>
2019-05-16 23:49:16 +0900

    [Add] Search

---
+++ src/components/SearchForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const SearchForm = ({history}) => {
+  let input;
+
+  const handleClick = (e, input) => {
+    e.preventDefault();
+    history.push({
+      pathname: "/search",
+      keyword: input
+    });
+  };
+
+  return (
+    <form>
+      <label htmlFor="keyword">キーワード</label>
+      <input id="keyword" type="text" ref={node => input = node}/>
+      <button type="submit" onClick={(e) => handleClick(e, input.value)}>検索</button>
+    </form>
+  );
+};
---
+++ src/containers/Search.js
@@ -0,0 +1,21 @@
+import React, {Fragment} from 'react';
+import {connect} from "react-redux";
+import {SearchForm} from "../components/SearchForm";
+import {ItemList} from "../components/ItemList";
+
+const Search = ({items, keyword, history}) => {
+  const searchResult = items.filter(e => e.title === keyword);
+
+  return (
+    <Fragment>
+      <SearchForm history={history}/>
+      <ItemList items={searchResult}/>
+    </Fragment>
+  );
+};
+
+export default connect(
+  state => ({
+    items: state.itemReducer.items,
+  })
+)(Search);
