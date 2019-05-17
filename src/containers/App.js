f6431e6384515761525c054fb506b6a6e4d76774
katoyoshiharu@team-lab.com <katoyoshiharu@team-lab.com>
2019-05-17 21:49:55 +0900

    [Fix] Navコンポーネントをnavタグで囲む

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Route, Switch} from "react-router-dom";
 import {Nav} from "../components/Nav";
 import ItemList from "../containers/ItemList";
@@ -9,35 +9,39 @@ import Edit from "../containers/Edit";
 
 export const App = () => {
   return (
-    <main>
-      <Route path="/" component={Nav}/>
-      <Switch>
-        <Route exact path="/" component={ItemList}/>
-        <Route
-          exact path="/new"
-          render={
-            ({history}) => <New history={history}/>
-          }
-        />
-        <Route
-          exact path="/search"
-          render={
-            ({location, history}) => <Search keyword={location.keyword} history={history}/>
-          }
-        />
-        <Route
-          exact path="/:id"
-          render={
-            ({match}) => <Item itemId={match.params.id}/>
-          }
-        />
-        <Route
-          exact path="/:id/edit"
-          render={
-            ({match, history}) => <Edit itemId={match.params.id} history={history}/>
-          }
-        />
-      </Switch>
-    </main>
+    <Fragment>
+      <nav>
+        <Route path="/" component={Nav}/>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/" component={ItemList}/>
+          <Route
+            exact path="/new"
+            render={
+              ({history}) => <New history={history}/>
+            }
+          />
+          <Route
+            exact path="/search"
+            render={
+              ({location, history}) => <Search keyword={location.keyword} history={history}/>
+            }
+          />
+          <Route
+            exact path="/:id"
+            render={
+              ({match}) => <Item itemId={match.params.id}/>
+            }
+          />
+          <Route
+            exact path="/:id/edit"
+            render={
+              ({match, history}) => <Edit itemId={match.params.id} history={history}/>
+            }
+          />
+        </Switch>
+      </main>
+    </Fragment>
   );
 };
