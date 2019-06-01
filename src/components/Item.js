131255e497523262860d2f45a8de224a7fd71fac
加藤 義晴 <katoyoshiharu@team-lab.com>
2019-06-01 18:39:10 +0900

    [Fix] Showの編集ボタンと削除ボタンをItemコンポーネントに移動し、編集ボタンと削除ボタンにデザインを当てる

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {Paper, Table, TableBody, TableCell, TableRow} from "@material-ui/core";
+import {Button, Paper, Table, TableBody, TableCell, TableRow} from "@material-ui/core";
 
-export const Item = ({item}) => {
+export const Item = ({history, item, deleteRequest}) => {
+  const id = item.id;
+
+  const handleClickToEdit = e => {
+    e.preventDefault();
+    history.push(`/items/${id}/edit`);
+  };
+
+  const handleClickDeleteRequest = e => {
+    e.preventDefault();
+    deleteRequest(id);
+  };
+
   return (
     <Paper>
-      <h1>{item.title}</h1>
+      <div>
+        <h1>{item.title}</h1>
+        <Button onClick={e => handleClickToEdit(e)}>編集</Button>
+        <Button color="primary" onClick={e => handleClickDeleteRequest(e)}>削除</Button>
+      </div>
       <div>
         <img src={item.imageSrc} alt="商品画像"/>
       </div>
