aaa1854699c0f087033c028155cbaf885c108ae3
加藤 義晴 <katoyoshiharu@team-lab.com>
2019-06-01 17:02:18 +0900

    [Add] Itemコンポーネントにデザインを当てる

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import {Paper, Table, TableBody, TableCell, TableRow} from "@material-ui/core";
 
 export const Item = ({item}) => {
   return (
-    <table>
-      <tbody>
-      <tr>
-        <td>商品タイトル</td>
-        <td>{item.title}</td>
-      </tr>
-      <tr>
-        <td>商品説明</td>
-        <td>{item.description}</td>
-      </tr>
-      <tr>
-        <td>価格</td>
-        <td>{item.price}</td>
-      </tr>
-      <tr>
-        <td>商品画像</td>
-        <td><img src={item.imageSrc} alt="商品画像"/></td>
-      </tr>
-      </tbody>
-    </table>
+    <Paper>
+      <h1>{item.title}</h1>
+      <div>
+        <img src={item.imageSrc} alt="商品画像"/>
+      </div>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell>説明文</TableCell>
+            <TableCell>{item.description}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>価格</TableCell>
+            <TableCell>¥{item.price}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </Paper>
   );
 };
