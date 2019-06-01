f861b196d3dd96dcfba1a5d040c60eea8af0271d
加藤 義晴 <katoyoshiharu@team-lab.com>
2019-06-01 18:22:16 +0900

    [Add] Navコンポーネントにデザインを当てる

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import {indexRequest} from "../actions/requests";
 import {setItems} from "../actions/items";
+import {AppBar, Button, Toolbar, Typography} from "@material-ui/core";
 
 const Nav = ({history, indexRequest, setItems}) => {
   const handleClickToIndex = e => {
     e.preventDefault();
     indexRequest();
+  };
+
+  const handleClickToNew = e => {
+    e.preventDefault();
+    history.push("/items/new");
   };
 
   const handleClickToSearch = e => {
@@ -16,13 +21,22 @@ const Nav = ({history, indexRequest, setItems}) => {
     history.push("/items/search")
   };
 
+  const handleClickToAuth = e => {
+    e.preventDefault();
+    history.push("/auth");
+  };
+
   return (
-    <nav>
-      <a onClick={e => handleClickToIndex(e)}>商品一覧ページへ</a>
-      <Link to="/items/new">新規登録ページへ</Link>
-      <a onClick={e => handleClickToSearch(e)}>検索ページへ</a>
-      <Link to="/auth">認証ページへ</Link>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <Button color="inherit" onClick={e => handleClickToIndex(e)}>
+          <Typography>商品管理アプリ</Typography>
+        </Button>
+        <Button color="inherit" onClick={e => handleClickToNew(e)}>新規登録</Button>
+        <Button color="inherit" onClick={e => handleClickToSearch(e)}>検索</Button>
+        <Button color="inherit" onClick={e => handleClickToAuth(e)}>認証</Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
