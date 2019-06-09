137e1124d0dcb0dad10776c914e89894c8f937cd
加藤 義晴 <katoyoshiharu@team-lab.com>
2019-06-10 00:45:02 +0900

    [Fix] エラーメッセージと画像

--- src/containers/ErrorMessage.js
+++ src/containers/ErrorMessage.js
@@ -1,29 +1,58 @@
 import React, {Fragment} from 'react';
 import {connect} from "react-redux";
+import {makeStyles} from '@material-ui/core/styles';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+
+const useStyles = makeStyles(theme => ({
+  error: {
+    backgroundColor: theme.palette.error.dark,
+    margin: theme.spacing(1)
+  },
+  icon: {
+    fontSize: 20,
+    opacity: 0.9,
+    marginRight: theme.spacing(1)
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  list: {
+    listStyle: 'square',
+    marginLeft: 25
+  }
+}));
 
 const ErrorMessage = ({message, details}) => {
+  const classes = useStyles();
+
+  if (!message) {
+    return null;
+  }
+
   return (
-    <Fragment>
-      {
-        message && (
-          <Fragment>
-            <p>{message}</p>
-            {
-              details && (
-                <ul>
-                  {
-                    details.map((detail, index) => {
-                      return <li key={index}>{detail}</li>;
-                    })
-                  }
-                </ul>
-              )
-            }
-          </Fragment>
-        )
+    <SnackbarContent
+      className={classes.error}
+      aria-describedby="client-snackbar"
+      message={
+        <Fragment>
+          <p className={classes.message}>
+            {message}
+          </p>
+          {
+            details !== undefined && details !== null &&
+            <ul>
+              {
+                Object.values(details).map((value, index) => {
+                  return <li key={index} className={classes.list}>{value}</li>;
+                })
+              }
+            </ul>
+          }
+        </Fragment>
       }
-    </Fragment>
-  )
+    />
+  );
 };
 
 export default connect(
